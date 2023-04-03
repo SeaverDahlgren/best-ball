ef6e9fa1c60ff711e837e5f2ab5fce0cac1eafc6
Seaver Dahlgren <12dahlgren12@gmail.com>
2023-04-02 20:13:09 -0400

    Fixed bug that prevented reassigning a ball and added ball types and changed model a bit

--- bestball2/bestballapp/forms.py
+++ bestball2/bestballapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class getPlayerInfo(forms.Form):
-    player1 = forms.CharField(label='Player 1', max_length=100)
-    player2 = forms.CharField(label='Player 2', max_length=100)
-    player3 = forms.CharField(label='Player 3', max_length=100)
+    player1 = forms.CharField(label='Callaway', max_length=100)
+    player2 = forms.CharField(label='Velocity', max_length=100)
+    player3 = forms.CharField(label='Titleist', max_length=100)
